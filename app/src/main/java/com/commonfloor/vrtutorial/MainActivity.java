cc24e51875e292365b482da368ced79cebf3ba1b
Apurv <apurv.mmmec@gmail.com>
2015-12-15 00:33:25 +0530

    step-02

--- app/src/main/java/com/commonfloor/vrtutorial/MainActivity.java
+++ app/src/main/java/com/commonfloor/vrtutorial/MainActivity.java
@@ -1,16 +1,25 @@
 package com.commonfloor.vrtutorial;
 
-import android.app.Activity;
 import android.os.Bundle;
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
 
 
+public class MainActivity extends CardboardActivity {
 
-public class MainActivity extends Activity {
+    private VRRenderer mRenderer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRestoreGLStateEnabled(false);
+
+        mRenderer = new VRRenderer(this);
+        cardboardView.setRenderer(mRenderer);
+        setCardboardView(cardboardView);
 
     }
 
---
+++ app/src/main/java/com/commonfloor/vrtutorial/VRRenderer.java
@@ -0,0 +1,83 @@
+package com.commonfloor.vrtutorial;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
+import android.util.Log;
+
+
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+/**
+ * Created by apurv on 12/8/15.
+ */
+public class VRRenderer implements CardboardView.StereoRenderer{
+
+
+    public VRRenderer(Context context){
+
+
+    }
+
+    /**
+     * Creates the buffers we use to store information about the 3D world.
+     *
+     * <p>OpenGL doesn't use Java arrays, but rather needs data in a format it can understand.
+     * Hence we use ByteBuffers.
+     *
+     * @param config The EGL configuration used when creating the surface.
+     */
+    @Override
+    public void onSurfaceCreated(EGLConfig config) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int width, int height) {
+
+    }
+
+    /**
+     * Prepares OpenGL ES before we draw a frame.
+     *
+     * @param headTransform The head transformation in the new frame.
+     */
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    /**
+     * Draws a frame for an eye.
+     *
+     * @param eye The eye to render. Includes all required transformations.
+     */
+    @Override
+    public void onDrawEye(Eye eye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+}
